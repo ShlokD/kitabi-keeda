fae1066fa7cb95f9046ab25bc9ce37c14c1e93c4
Shlok Desai <shlokpdesai@gmail.com>
2020-04-12 15:18:26 -0700

    Add homepage search bar

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,13 +1,17 @@
 describe("Home Page", () => {
-  it("renders a search bar with image and title", () => {
+  before(() => {
     cy.visit("localhost:5000");
-    const searchBar = cy.get("[data-test=search-bar]");
-    searchBar.should("have.length", 1);
-    searchBar.get("[data-test=search-icon]").should("have.length", 1);
-    searchBar.get("[data-test=page-title]").should("have.length", 1);
+  });
+
+  it("renders a search bar with image and title", () => {
+    cy.get("[data-test=page-title]").should("have.length", 1);
   });
 
   it("renders a featured image", () => {
     cy.get("[data-test=featured-image").should("have.length", 1);
+  });
+
+  it("renders search input", () => {
+    cy.get("[data-test=search-input]").should("have.length", 1);
   });
 });
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,38 +1,36 @@
 import { Component } from "preact";
 import { connect } from "unistore/preact";
 import actions from "./domain/actions";
-import MagnifyingGlass from "../../assets/magnifying-glass.svg";
-import FeaturedImageCarousel from "../../assets/books.jpg";
 
-const SearchBar = () => (
-  <div
-    data-test="search-bar"
-    className="flex justify-center items-center moon-gray w-100"
-  >
-    <img
-      data-test="search-icon"
-      className="ma2"
-      alt="Search for Books"
-      src={MagnifyingGlass}
-    />
-    <h1 data-test="page-title" className="f3 tc ma2">
-      KITABI KEEDA
-    </h1>
-  </div>
-);
+const SearchBar = () => {
+  return (
+    <input
+      className={`b b--transparent br-pill pa3 ma2 animate-opacity w-80 search-bar moon-gray`}
+      data-test="search-input"
+      placeholder="Search for titles, authors, topics..."
+    ></input>
+  );
+};
+
 const FeaturedImage = () => (
-  <img data-test="featured-image" src={FeaturedImage} />
+  <img
+    className="w-100 w-80-ns pa2 shadow-4"
+    data-test="featured-image"
+    src="https://images.unsplash.com/photo-1568047571827-8c46fe611345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+  />
 );
-const SearchText = () => <div>Search For Books To Get Started</div>;
+
 const BooksList = () => <div>List of books here</div>;
 
 class Home extends Component {
   render({ books }) {
     return (
-      <div className="flex flex-column h-100 pa4 items-center">
-        <SearchBar />
+      <div className="flex flex-column h-100 items-center pa3">
+        <h1 data-test="page-title" className="f2 tc ma2">
+          KITABI KEEDA
+        </h1>
         <FeaturedImage />
-        <SearchText />
+        <SearchBar />
         <BooksList />
       </div>
     );
